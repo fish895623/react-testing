b1e7b3ef7b2f67549f4608dd7dd4b0c52c2b66ae
fish895623 <dan990429@gmail.com>
2022-03-20 01:57:48 +0900

    Use interface to render array

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import create from "zustand";
-import axios from "axios";
 import "./App.css";
+import { useEffect } from "react";
+import { Person } from "./interfaces/Person";
 
-const useStore = create((set) => ({
+const useStore = create((set: any) => ({
   count: 0,
+  hello: [],
+  helloa: (data: any) => {
+    set({ hello: data });
+  },
 }));
 
+async function T() {
+  const response = await fetch("https://gorest.co.in/public/v2/users");
+  const finish = await response.json();
+  return finish;
+}
+
 function App() {
-  const { count } = useStore();
+  const { count, hello, helloa } = useStore();
+  useEffect(() => {
+    T().then((res) => {
+      helloa(res);
+    });
+  }, [helloa]);
+
   return (
     <div className="App">
       <div className="App-header">
@@ -16,6 +33,11 @@ function App() {
         <p className="Testing">Classname Works Fine</p>
         <p className="TestingContent">Hello</p>
         <p>{count}</p>
+        {hello.map((home: Person) => (
+          <div>
+            {home.id}, {home.email}, {home.gender}, {home.status}
+          </div>
+        ))}
       </div>
     </div>
   );
