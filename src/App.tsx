b03f3b9661e974fac7455037ca9717ee836af197
fish895623 <dan990429@gmail.com>
2022-03-19 23:47:25 +0900

    zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import create from "zustand";
 import "./App.css";
 
+const useStore = create((set) => ({
+  count: 0,
+}));
+
 function App() {
+  const { count } = useStore();
   return (
     <div className="App">
       <div className="App-header">
-        <div className="T">
-          <p className="Testing">Classname Works Fine</p>
-          <p className="TestingContent">Hello</p>
-        </div>
+        <div className="Title"></div>
+        <div className="Content"></div>
+        <p className="Testing">Classname Works Fine</p>
+        <p className="TestingContent">Hello</p>
+        <p>{count}</p>
       </div>
     </div>
   );
