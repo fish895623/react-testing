d04752d62e02b688b3175a0b1c5e9ca3b5c29ca8
fish895623 <dan990429@gmail.com>
2022-03-20 22:10:04 +0900

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,31 @@ import { Person } from "./interfaces/Person"
 const useStore = create((set: any) => ({
   count: 0,
   hello: [],
-  helloa: (data: any) => {
+  setHello: (data: any) => {
     set({ hello: data })
   },
 }))
 
-async function T() {
+async function fetching() {
   const response = await fetch("https://gorest.co.in/public/v2/users")
   const finish = await response.json()
   return finish
 }
 
 function App() {
-  const { count, hello, helloa } = useStore()
+  const { hello, setHello } = useStore()
+
   useEffect(() => {
-    T().then((res) => {
-      helloa(res)
+    fetching().then((res) => {
+      setHello(res)
     })
-  }, [helloa])
+  }, [setHello])
 
   return (
     <div className="App">
       <div className="App-header">
         <div className="Title"></div>
         <div className="Content"></div>
-        <p className="Testing">Classname Works Fine</p>
-        <p className="TestingContent">Hello</p>
-        <p>{count}</p>
         {hello.map((home: Person) => (
           <div>
             {home.id}, {home.email}, {home.gender}, {home.status}
